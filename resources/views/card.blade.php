3313c3cf966c499ce9b2ccb664febb4202daeec4
天阳 <tianyang@test.com>
2020-11-25 22:54:55 +0700

    edit

---
+++ resources/views/card.blade.php
@@ -0,0 +1,10 @@
+<div class="box box-default">
+    <div class="info-box">
+        <span class="info-box-icon bg-{{$color}}"><i class="fa {{$icon}}"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text" style="font-size: large">{{$name}}</span>
+            <span class="info-box-number">{{$value}}</span>
+        </div>
+    </div>
+</div>
